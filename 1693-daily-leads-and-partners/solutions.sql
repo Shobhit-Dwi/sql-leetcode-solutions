544065647228986e32f866424a814f205af5c7fe
Shobhit Dwivedi <225417142+Shobhit-Dwi@users.noreply.github.com>
2025-08-20 14:29:46 +0530

    Create solutions.sql

---
+++ 1693-daily-leads-and-partners/solutions.sql
@@ -0,0 +1,6 @@
+SELECT TO_CHAR(date_id, 'YYYY-MM-DD') AS date_id, 
+       make_name, 
+       COUNT(DISTINCT lead_id) AS unique_leads, 
+       COUNT(DISTINCT partner_id) AS unique_partners
+FROM DailySales
+GROUP BY date_id, make_name;
